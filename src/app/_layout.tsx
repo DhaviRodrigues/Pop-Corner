ad8e0cba7d31163cd0df9a5dd65882cb6119e24d
DhaviRodrigues <dhavirodrigues2@gmail.com>
2026-04-21 17:26:16 -0300

    fix: layout.tsx

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,21 +2,15 @@ import { UserProvider } from '@/contexts/UserContext';
 import { UserRegistrationProvider } from '@/contexts/UserRegistrationContext';
 import {
   Poppins_300Light,
-  Poppins_400Regular, Poppins_600SemiBold, Poppins_700Bold,
-  useFonts
-} from '@expo-google-fonts/poppins';
-import { Stack } from "expo-router";
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-import * as SplashScreen from "expo-splash-screen";
-import { useEffect } from "react";
-import Head from "expo-router/head";
-import {
-  useFonts,
   Poppins_400Regular,
   Poppins_600SemiBold,
   Poppins_700Bold,
-} from "@expo-google-fonts/poppins";
+  useFonts
+} from '@expo-google-fonts/poppins';
+import { Stack } from 'expo-router';
+import Head from 'expo-router/head';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
 
 SplashScreen.preventAutoHideAsync();
 
@@ -41,56 +35,51 @@ export default function RootLayout() {
   return (
     <UserProvider>
       <UserRegistrationProvider>
+        <Head>
+          <title>PopCorner</title>
+          <link
+            rel="stylesheet"
+            href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
+            integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
+            crossOrigin=""
+          />
+
+          <style>{`
+            .leaflet-popup-content-wrapper {
+              background: transparent !important;
+              box-shadow: none !important;
+              padding: 0 !important;
+            }
+            .leaflet-popup-tip-container {
+              display: none !important;
+            }
+            .leaflet-popup-content {
+              margin: 0 !important;
+            }
+            .leaflet-popup-close-button {
+              display: none !important;
+            }
+            .leaflet-top.leaflet-left {
+              top: 620px !important; 
+              margin-left: 10px !important;
+            }
+
+            .leaflet-bar {
+              border: 2px solid #FFFEB2 !important;
+              box-shadow: none !important;
+            }
+            .leaflet-bar a {
+              background-color: rgba(0, 0, 0, 0.8) !important;
+              color: #FFFEB2 !important;
+              border-bottom: 1px solid #444 !important;
+            }
+          `}</style>
+        </Head>
+
         <Stack screenOptions={{ headerShown: false, animation: 'fade' }}>
           <Stack.Screen name="index" />
         </Stack>
       </UserRegistrationProvider>
     </UserProvider>
-    <>
-      <Head>
-        <title>PopCorner</title>
-        <link
-          rel="stylesheet"
-          href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
-          integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
-          crossOrigin=""
-        />
-
-        <style>{`
-          .leaflet-popup-content-wrapper {
-            background: transparent !important;
-            box-shadow: none !important;
-            padding: 0 !important;
-          }
-          .leaflet-popup-tip-container {
-            display: none !important;
-          }
-          .leaflet-popup-content {
-            margin: 0 !important;
-          }
-          .leaflet-popup-close-button {
-            display: none !important;
-          }
-          .leaflet-top.leaflet-left {
-          top: 620px !important; 
-          margin-left: 10px !important;
-          }
-
-          .leaflet-bar {
-            border: 2px solid #FFFEB2 !important; /* Borda amarela */
-            box-shadow: none !important;
-          }
-          .leaflet-bar a {
-            background-color: rgba(0, 0, 0, 0.8) !important;
-            color: #FFFEB2 !important;
-            border-bottom: 1px solid #444 !important;
-          }
-        `}</style>
-      </Head>
-
-      <Stack screenOptions={{ headerShown: false, animation: "fade" }}>
-        <Stack.Screen name="index" />
-      </Stack>
-    </>
   );
 }
